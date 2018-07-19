8735d079fa27239000e6915646e5ac46a5c661e7
AndrushaLOL <takemyenergy55@gmail.com>
2018-07-19 18:42:57 +0500

    First
    Full app

---
+++ client/src/components/Register.vue
@@ -0,0 +1,53 @@
+<template>
+	<v-layout row>
+		<v-flex xs6 offset-xs3>
+		  <div class='white elevation-2'>
+		  	<v-toolbar flat dense class = "cyan" dark>
+		  	  <v-toolbar-title>Register</v-toolbar-title>
+		  	</v-toolbar>
+	  	  <div class="pl-4 pr-4">
+	  	  <v-text-field
+	  	    label="email"
+	  	    v-model="email"
+	  	  ></v-text-field>
+	  	  <v-text-field
+	  	    label="password"
+	  	    v-model="password"
+	  	  ></v-text-field>
+	  	  <v-btn color="blue lighten-4">Success</v-btn>
+	  	  </div>
+		  </div>
+		</v-flex>
+	  
+	</v-layout>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+	name: 'Register',
+	data () {
+		return {
+			email: '',
+			password: '',
+			error: null
+		}
+	},
+	methods: {
+		async Register () {
+			try {
+			await AuthenticationService.register({
+				email: this.email,
+				password: this.password
+			})
+
+			this.email = ''
+			this.password = ''
+			} catch (e) {
+				this.error = e.response.data.error
+			}
+		}
+	}
+}
+</script>
---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,36 @@
+const Joi = require('joi')
+
+module.exports = {
+	register(req, res, next) {
+		const schema = {
+			email: Joi.string().email(),
+			password: Joi.string().regex(
+				new RegExp('^[a-zA-Z0-9]{3,32}$')
+			)
+		}
+
+		const {error, value} = Joi.validate(req.body, schema)
+
+		if (error) {
+ 			switch (error.details[0].context.key) {
+ 				case 'email':
+ 					res.status(400).send({
+ 						message: 'Enter correct email (example@example.com)'
+ 					})
+ 					break
+ 				case 'password':
+ 					res.status(400).send({
+ 						message: 'password must be between 8 and 32 symbols and contains letters or digits'
+ 					})
+ 					break
+ 				default:
+ 					res.status(400).send({
+ 						message: 'server error'
+ 					})
+ 			}
+
+		} else {
+			next()
+		}
+	}
+}
